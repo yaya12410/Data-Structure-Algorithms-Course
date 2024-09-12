95d2bce2a3b384726da590d56245a22e4f0661e7
yaya12410 <ymabdelbarr@gmail.com>
2024-09-12 12:29:45 +0300

    Converting from infix to postfix expression

--- Stack/StackUsingLL.c
+++ Stack/StackUsingLL.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct Node 
 {
@@ -75,9 +76,66 @@ int isBalanced(char *exp)
     return 0;
 }
 
+int pre(char x)
+{
+    if(x=='+' || x=='-')
+    {
+        return 1;
+    }
+    else if(x=='*' || x=='/')
+    {
+        return 2;
+    }
+    return 0;
+}
+
+int isOperand(char x)
+{
+    if(x=='+' || x=='-' || x=='*' || x=='/')
+    {
+        return 0;
+    }
+    return 1;
+}
+
+char *intoPost(char *infix)
+{
+    int i=0, j=0;
+    char *postfix;
+    int len=strlen(infix);
+    postfix=(char *)malloc((len+2)*sizeof(char));
+    while(infix[i]!='\0')
+    {
+        if(isOperand(infix[i]))
+        {
+            postfix[j++]=infix[i++];
+        }
+        else
+        {
+            if(pre(infix[i])>pre(top->data))
+            {
+                push(infix[i++]);
+            }
+            else
+            {
+                postfix[j++]=pop();
+            }
+        }
+    }
+    while(top)
+    {
+        postfix[j++]=pop();
+    }
+    postfix[j]='\0';
+    return postfix;
+}
+
 int main()
 {
-    char *exp="((a+b)*(c-d)))";
-    printf("%d", isBalanced(exp));
+    char *infix="a+b*c-d/e";
+    push('#');
+    char *postfix=intoPost(infix);
+
+    printf("%s", postfix);
     return 0;
 }

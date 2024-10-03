8cb18842d2b5336dd857ae1d42ffede71eaf8e9b
yaya12410 <ymabdelbarr@gmail.com>
2024-10-03 03:21:57 +0300

    Selection Sort and Quick Sorting techniques

--- Sorting Techniques/Bubble_Sort.c
+++ Sorting Techniques/Bubble_Sort.c
@@ -44,11 +44,61 @@ void InsertionSort(int Arr[], int num)
     }
 }
 
+void SelectionSort(int Arr[], int num)
+{
+    int i, j, k;
+    for(i=0; i<num-1; i++)
+    {
+        for(j=k=i; j<num; j++)
+        {
+            if(Arr[j]<Arr[k])
+            {
+                k=j;
+            }
+        }
+        swap(&Arr[i], &Arr[k]);
+    }
+}
+
+int Partition(int Arr[], int low, int high)
+{
+    int pivot=Arr[low];
+    int i=low, j=high;
+    do
+    {
+        do
+        {
+            i++;
+        }while(Arr[i]<=pivot);
+        do
+        {
+            j--;
+        }while(Arr[j]>pivot);
+        if(i<j)
+        {
+            swap(&Arr[i], &Arr[j]);
+        }
+    }while(i<j);
+    swap(&Arr[low], &Arr[j]);
+    return j;
+}
+
+void QuickSort(int Arr[], int low, int high)
+{
+    int j;
+    if(low<high)
+    {
+        j=Partition(Arr, low, high);
+        QuickSort(Arr, low, j);
+        QuickSort(Arr, j+1, high);
+    }
+}
+
 int main()
 {
-    int Array[]={5, 2, 1, 7, 23, 78, 12};
-    int n=7;
-    InsertionSort(Array, n);
+    int Array[]={11, 13, 7, 12, 16, 9, 24, 5, 10, 3, __INT32_MAX__};
+    int n=11;
+    QuickSort(Array, 0, 10);
     for(int i=0; i<n; i++)
     {
         printf("%d ", Array[i]);
